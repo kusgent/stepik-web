e59d994425e39763bd2ef90a0f3fbf4ff865016b
Kustov Ilya <kusgent@yandex.ru>
2022-08-31 21:48:47 +0300

    Add ex_35

---
+++ ex_35/main.js
@@ -0,0 +1,5 @@
+let elem = document.getElementById('btn');
+let elem1 = document.getElementsByClassName('btn');
+elem.style.background = 'red';
+console.log(typeof elem);
+console.log(typeof elem1);
